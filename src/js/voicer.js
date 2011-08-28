753396471187c127800e6129db0ef00ea94a34e0
Alexander Yatkevich <a.yatkevich@gmail.com>
2011-08-28 17:35:17 +0700

    Voicer changes

--- src/js/voicer.js
+++ src/js/voicer.js
@@ -1,24 +1,26 @@
-$(function(){
-	if (!localStorage["lang"]) {
-		localStorage["lang"] = 'en';
-	}
-	setInterval(function(){
-		var textareas = $('textarea[has_mic!=true]');
-		console.log(textareas.length);
-		textareas.each(function(i) {
-			$this = $(this);
-			if (!$this.attr('has_mic')) {
-				var str = '<input type="text" class="mic-for-textarea"  style="width: 25px; height: 25px; font-size: 25px; cursor: pointer; border: none; position: absolute; margin-left: -27px; margin-top: 1px; outline: none; background: transparent;" x-webkit-speech id="mic_'+i+'_'+this.id+'" lang="' + localStorage["lang"] + '" />';
-				var $input = $(str);
-				$input.insertAfter(this);
-				var input = $input[0];
-				input.onfocus = input.blur;
-				input.onwebkitspeechchange = function(e) {
-				    console.log(e); // SpeechInputEvent
-				    this.previousSibling.value = this.value;
-				};
-				$this.attr('has_mic', true);
+var contentScript = {
+    	file: '/js/content-script.js',
+    	allFrames: true
+    },
+    tabCallbacks = [
+    	chrome.extension.onRequest,
+    	chrome.tabs.onSelectionChanged,
+    	chrome.tabs.onUpdated
+    ];
+
+tabCallbacks.forEach(function(callback, callbackId) {
+	callback.addListener(function(tabId) {
+		var tabGetter = chrome.tabs.get.bind(null, tabId);
+		if (typeof tabId !== 'number') {
+			tabGetter = chrome.tabs.getCurrent;
+		}
+		tabGetter(function(tab) {
+			if (! /^http(|s)/.test(tab.url) || tab.status != 'complete') {
+				return;
 			}
+			chrome.tabs.executeScript(tab.id, contentScript, function() {
+				console.log('fired!', tab.url, arguments);
+			});
 		});
-	}, 2000);
+	});
 });
